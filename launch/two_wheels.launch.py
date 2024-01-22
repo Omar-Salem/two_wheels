49a06f7ca49863a4df37be488f860875fa4cedf3
omar salem <omar.gamil.salem@gmail.com>
2024-01-22 12:28:16 +1100

    refactor

--- launch/two_wheels.launch.py
+++ launch/two_wheels.launch.py
@@ -9,17 +9,12 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name = 'two_wheels'  # <--- CHANGE ME
+    package_name = 'two_wheels'
 
     pkg_path = os.path.join(get_package_share_directory(package_name))
     xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
-    # robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = Command(['xacro ', xacro_file])
 
-    # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config,
               'use_sim_time': True}
     robot_state_pub_node = Node(
@@ -29,38 +24,40 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+    gazebo_nodes = create_gazebo_nodes(package_name)
 
+    controller_nodes = create_controller_nodes()
+    return LaunchDescription([
+                                 robot_state_pub_node,
+                             ] + gazebo_nodes
+                             + controller_nodes)
+
+
+def create_controller_nodes():
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"],
+    )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+    return [joint_broad_spawner, diff_drive_spawner]
+
+
+def create_gazebo_nodes(package_name):
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
         launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )
-
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_drive_controller"],
-    )
-
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"],
-    )
-
-    # Launch them all!
-    return LaunchDescription([
-        robot_state_pub_node,
-        gazebo,
-        spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
-    ])
+    return [gazebo, spawn_entity]
