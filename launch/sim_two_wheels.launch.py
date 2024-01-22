517876c50987c36cabd92066976c7ab92cea32ac
omar salem <omar.gamil.salem@gmail.com>
2024-01-22 18:33:10 +1100

    removed gazebo_params_file

--- launch/sim_two_wheels.launch.py
+++ launch/sim_two_wheels.launch.py
@@ -90,15 +90,11 @@ def create_gazebo_nodes(package_name) -> list:
     world = PathJoinSubstitution(
         [package_dir, 'worlds', 'traffic_cones.world']
     )
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
-    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-        launch_arguments={'world': world,
-                          'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+        launch_arguments={'world': world}.items()
     )
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
