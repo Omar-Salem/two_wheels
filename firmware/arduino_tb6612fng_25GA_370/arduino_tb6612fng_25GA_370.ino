6e9097c458c16eae8dbb9bc36cf086630cc6b60a
omar salem <omar.gamil.salem@gmail.com>
2024-01-24 14:22:24 +1100

    measuring motor linear velocity

--- firmware/arduino_tb6612fng_25GA_370/arduino_tb6612fng_25GA_370.ino
+++ firmware/arduino_tb6612fng_25GA_370/arduino_tb6612fng_25GA_370.ino
@@ -17,21 +17,46 @@ Yellow Wire - signal feedback (motor one turn has 11 signals)
 Green Wire - signal feedback (motor one turn has 11 signals)
 
 **/
+//https://automaticaddison.com/how-to-calculate-the-velocity-of-a-dc-motor-with-encoder/
 
 //motor A connected between A01 and A02
 //motor B connected between B01 and B02
 
 
 #define BAUDRATE 9600
+// Motor encoder output pulses per 360 degree revolution (measured manually)
+#define ENC_COUNT_REV 105
+#define WHEEL_RADIUS_METERS 0.0349
+
+#define rpm_to_radians 0.10471975512
+
+
 //Motor A
-int PWMA = 3;  //Speed control
-int AIN1 = 8;  //Direction
-int AIN2 = 9;  //Direction
+#define PWMA 3  //Speed control
+#define AIN1 8  //Direction
+#define AIN2 9  //Direction
+#define ENC_IN_RIGHT_A 2
+volatile long motor_A_pulse_count = 0;
 
 //Motor B
-int PWMB = 5;   //Speed control
-int BIN1 = 11;  //Direction
-int BIN2 = 12;  //Direction
+#define PWMB 5   //Speed control
+#define BIN1 11  //Direction
+#define BIN2 12  //Direction
+
+
+// One-second interval for measurements
+int interval = 1000;
+
+// Counters for milliseconds during interval
+long previousMillis = 0;
+long currentMillis = 0;
+
+// Variable for RPM measuerment
+float rpm_motor_A = 0;
+
+// Variable for angular velocity measurement
+float ang_velocity_motor_A = 0;
+float ang_velocity_motor_A_deg = 0;
 
 void setup() {
     Serial.begin(BAUDRATE);
@@ -39,6 +64,11 @@ void setup() {
     pinMode(PWMA, OUTPUT);
     pinMode(AIN1, OUTPUT);
     pinMode(AIN2, OUTPUT);
+    // Set pin states of the encoder
+    pinMode(ENC_IN_RIGHT_A, INPUT_PULLUP);
+
+    // Every time the pin goes high, this is a pulse
+    attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), motor_A_pulse, RISING);
 
     pinMode(PWMB, OUTPUT);
     pinMode(BIN1, OUTPUT);
@@ -85,4 +115,38 @@ void loop() {
 
         Serial.println("OK");
     }
+
+    // Record the time
+    currentMillis = millis();
+
+    // If one second has passed, print the number of pulses
+    if (currentMillis - previousMillis > interval) {
+
+        previousMillis = currentMillis;
+
+        // Calculate revolutions per minute
+        rpm_motor_A = (float) (motor_A_pulse_count * 60 / ENC_COUNT_REV);
+        ang_velocity_motor_A = rpm_motor_A * rpm_to_radians;
+
+        Serial.print(" Pulses: ");
+        Serial.println(motor_A_pulse_count);
+
+        Serial.print(" RPM: ");
+        Serial.print(rpm_motor_A);
+
+        Serial.print(" Angular Velocity: ");
+        Serial.print(ang_velocity_motor_A);
+        Serial.print(" rad per second");
+
+        Serial.print(" Linear Velocity: ");
+        Serial.print(WHEEL_RADIUS_METERS * ang_velocity_motor_A);
+        Serial.print(" meters per second");
+
+        motor_A_pulse_count = 0;
+
+    }
+}
+
+void motor_A_pulse() {
+    motor_A_pulse_count++;
 }
