76d68a82969892e4bc343b6346de029cef2b6f7f
omar salem <omar.gamil.salem@gmail.com>
2024-01-30 14:18:50 +1100

    logging

--- src/bump_go.cpp
+++ src/bump_go.cpp
@@ -37,9 +37,8 @@ private:
     rclcpp::Subscription<Range>::SharedPtr rangeTopicSubscription_;
 
     void rangeTopicCallback(const Range &range) {
-        RCLCPP_INFO(this->get_logger(), "min_range: '%s'", to_string(range.min_range).c_str());
-        RCLCPP_INFO(this->get_logger(), "max_range: '%s'", to_string(range.max_range).c_str());
-        RCLCPP_INFO(this->get_logger(), "range: '%s'", to_string(range.range).c_str());
+//        RCLCPP_INFO(this->get_logger(), "min_range: '%s'", to_string(range.min_range).c_str());
+//        RCLCPP_INFO(this->get_logger(), "max_range: '%s'", to_string(range.max_range).c_str());
         range_ = range;
     }
 
@@ -47,9 +46,22 @@ private:
 //        if (&range_ == nullptr) {
 //            return;
 //        }
+        RCLCPP_INFO(this->get_logger(), "range: '%s'", to_string(range_.range).c_str());
         auto message = TwistStamped();
-        message.twist.linear.x = 0.7;
-        message.twist.linear.z = 1.0;
+        //Clear, keep going
+        if (range_.range > 0.5) {
+            message.twist.linear.x = 0.1;
+            message.twist.linear.z = 1.0;
+        } else {
+
+            RCLCPP_INFO(this->get_logger(), "BLOCKED! Turning!.....");
+            //Turn left or right by random
+            int lb = 1, ub = 100;
+            auto chosen = (rand() % (ub - lb + 1)) + lb;
+
+            message.twist.linear.x = 0.7;
+            message.twist.linear.z = 2.0 * chosen < 50 ? 1 : -1;
+        }
 //        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
         twistStampedPublisher_->publish(message);
     }
