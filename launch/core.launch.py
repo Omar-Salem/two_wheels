e64dd6368bcdf0c8ff76e90e5590a97abeeafc2a
omar salem <omar.gamil.salem@gmail.com>
2024-02-03 14:02:42 +1100

    re-enabled bump and go node

--- launch/core.launch.py
+++ launch/core.launch.py
@@ -41,21 +41,11 @@ def create_robot_node() -> list:
             ),
             {'use_sim_time': is_sim}]
     )
-    # remap_diff_drive_controller_cmd_vel = Node(
-    #     package='turtlesim',
-    #     executable='mimic',
-    #     name='mimic',
-    #     remappings=[
-    #         ('/input/pose', '/turtlesim1/turtle1/pose'),
-    #         ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-    #     ]
-    # )
     bump_go = Node(
         package=LaunchConfiguration("package_name"),
         executable="bump_go",
         name="bump_go",
     )
     return [robot_localization,
-
-            # bump_go
+            bump_go
             ]
