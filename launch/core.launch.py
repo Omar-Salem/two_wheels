bb31841b5d1f4cd873586d07e69370ee97e20151
omar salem <omar.gamil.salem@gmail.com>
2024-02-03 13:41:48 +1100

    using ekf

--- launch/core.launch.py
+++ launch/core.launch.py
@@ -1,7 +1,9 @@
+import launch_ros
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -9,6 +11,9 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "package_name",
             description="Package name",
+        ), DeclareLaunchArgument(
+            "is_sim",
+            default_value="true",
         )]
 
     robot_nodes = create_robot_node()
@@ -23,9 +28,34 @@ def create_robot_node() -> list:
 
     :rtype: list
     """
+    package_name = LaunchConfiguration("package_name")
+    is_sim = LaunchConfiguration('is_sim')
+    robot_localization = launch_ros.actions.Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[
+            PathJoinSubstitution(
+                [FindPackageShare(package_name), "config", "ekf.yaml"]
+            ),
+            {'use_sim_time': is_sim}]
+    )
+    # remap_diff_drive_controller_cmd_vel = Node(
+    #     package='turtlesim',
+    #     executable='mimic',
+    #     name='mimic',
+    #     remappings=[
+    #         ('/input/pose', '/turtlesim1/turtle1/pose'),
+    #         ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
+    #     ]
+    # )
     bump_go = Node(
         package=LaunchConfiguration("package_name"),
         executable="bump_go",
         name="bump_go",
     )
-    return [bump_go]
+    return [robot_localization,
+
+            # bump_go
+            ]
