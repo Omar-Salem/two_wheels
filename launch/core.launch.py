9852c70c3fcbac33cebf06e9f7bd809160e225af
omar salem <omar.gamil.salem@gmail.com>
2024-02-04 20:44:55 +1100

    slam

--- launch/core.launch.py
+++ launch/core.launch.py
@@ -1,6 +1,10 @@
+import os
+
 import launch_ros
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -46,6 +50,18 @@ def create_robot_node() -> list:
         executable="bump_go",
         name="bump_go",
     )
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory("slam_toolbox"), 'launch', 'online_async_launch.py'
+        )])
+    )
+    nav2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory("nav2_bringup"), 'launch', 'navigation_launch.py'
+        )])
+    )
     return [robot_localization,
-            # bump_go
+            bump_go,
+            slam_toolbox,
+            nav2_bringup
             ]
