c663038238e2625b58af1088cea240f18ae601ed
omar salem <omar.gamil.salem@gmail.com>
2024-02-20 16:50:00 +1100

    costmap

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -17,6 +17,7 @@
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "map_msgs/msg/occupancy_grid_update.hpp"
+#include "nav2_costmap_2d/costmap_2d.hpp"
 #include "angles/angles.h"
 
 using std::placeholders::_1;
@@ -25,6 +26,7 @@ using geometry_msgs::msg::PoseWithCovarianceStamped;
 using geometry_msgs::msg::PoseStamped;
 using nav_msgs::msg::OccupancyGrid;
 using map_msgs::msg::OccupancyGridUpdate;
+using nav2_costmap_2d::Costmap2D;
 using angles::to_degrees;
 using angles::from_degrees;
 using std::to_string;
@@ -40,7 +42,11 @@ public:
     Mapper()
             : Node("mapper") {
         mapSubscription_ = this->create_subscription<OccupancyGrid>(
-                "/map", 10, std::bind(&Mapper::mapCallback, this, _1));
+                "/map", 10, std::bind(&Mapper::updateFullMap, this, _1));
+
+        mapUpdatesSubscription_ = this->create_subscription<OccupancyGridUpdate>(
+                "/map_updates", 10, std::bind(&Mapper::updatePartialMap, this, _1));
+
 
         poseSubscription_ = this->create_subscription<PoseWithCovarianceStamped>(
                 "/pose", 10, std::bind(&Mapper::poseTopicCallback, this, _1));
@@ -57,6 +63,7 @@ public:
     }
 
 private:
+    Costmap2D costmap_;
     /*
 # 0 represents unoccupied, 1 represents definitely occupied, and
 
@@ -80,24 +87,94 @@ private:
     vector<signed char> map;
     double originX, originY;
 
-    void mapCallback(OccupancyGrid::UniquePtr occupancyGrid) {
-        width = occupancyGrid->info.width;
-        map = occupancyGrid->data;
-        const auto origin = occupancyGrid->info.origin.position;
-        originX = origin.x;
-        originY = origin.y;
+    std::array<unsigned char, 256> init_translation_table() {
+        std::array<unsigned char, 256> cost_translation_table;
 
-        mapSubscription_.reset();//unsubscribe
+        // lineary mapped from [0..100] to [0..255]
+        for (size_t i = 0; i < 256; ++i) {
+            cost_translation_table[i] =
+                    static_cast<unsigned char>(1 + (251 * (i - 1)) / 97);
+        }
 
-        mapUpdatesSubscription_ = this->create_subscription<OccupancyGridUpdate>(
-                "/map_updates", 10, std::bind(&Mapper::mapUpdatesCallback, this, _1));
+        // special values:
+        cost_translation_table[0] = 0;      // NO obstacle
+        cost_translation_table[99] = 253;   // INSCRIBED obstacle
+        cost_translation_table[100] = 254;  // LETHAL obstacle
+        cost_translation_table[static_cast<unsigned char>(-1)] = 255;  // UNKNOWN
+
+        return cost_translation_table;
     }
 
-    void mapUpdatesCallback(OccupancyGridUpdate::UniquePtr occupancyGridUpdate) {
-        width = occupancyGridUpdate->width;
-        map = occupancyGridUpdate->data;
-        originX = occupancyGridUpdate->x;
-        originY = occupancyGridUpdate->y;
+    std::array<unsigned char, 256> cost_translation_table_ = init_translation_table();
+
+    void updateFullMap(OccupancyGrid::UniquePtr msg) {
+        unsigned int size_in_cells_x = msg->info.width;
+        unsigned int size_in_cells_y = msg->info.height;
+        double resolution = msg->info.resolution;
+        double origin_x = msg->info.origin.position.x;
+        double origin_y = msg->info.origin.position.y;
+
+        RCLCPP_INFO(this->get_logger(), "received full new map, resizing to: %d, %d", size_in_cells_x,
+                    size_in_cells_y);
+        costmap_.resizeMap(size_in_cells_x,
+                           size_in_cells_y,
+                           resolution,
+                           origin_x,
+                           origin_y);
+
+        // lock as we are accessing raw underlying map
+        auto *mutex = costmap_.getMutex();
+        std::lock_guard<Costmap2D::mutex_t> lock(*mutex);
+
+        // fill map with data
+        unsigned char *costmap_data = costmap_.getCharMap();
+        size_t costmap_size = costmap_.getSizeInCellsX() * costmap_.getSizeInCellsY();
+        RCLCPP_INFO(this->get_logger(), "full map update, %lu values", costmap_size);
+        for (size_t i = 0; i < costmap_size && i < msg->data.size(); ++i) {
+            unsigned char cell_cost = static_cast<unsigned char>(msg->data[i]);
+            costmap_data[i] = cost_translation_table_[cell_cost];
+        }
+    }
+
+    void updatePartialMap(OccupancyGridUpdate::UniquePtr msg) {
+        RCLCPP_DEBUG(this->get_logger(), "received partial map update");
+        if (msg->x < 0 || msg->y < 0) {
+            RCLCPP_ERROR(this->get_logger(), "negative coordinates, invalid update. x: %d, y: %d", msg->x,
+                         msg->y);
+            return;
+        }
+
+        size_t x0 = static_cast<size_t>(msg->x);
+        size_t y0 = static_cast<size_t>(msg->y);
+        size_t xn = msg->width + x0;
+        size_t yn = msg->height + y0;
+
+        // lock as we are accessing raw underlying map
+        auto *mutex = costmap_.getMutex();
+        std::lock_guard<Costmap2D::mutex_t> lock(*mutex);
+
+        size_t costmap_xn = costmap_.getSizeInCellsX();
+        size_t costmap_yn = costmap_.getSizeInCellsY();
+
+        if (xn > costmap_xn || x0 > costmap_xn || yn > costmap_yn ||
+            y0 > costmap_yn) {
+            RCLCPP_WARN(this->get_logger(), "received update doesn't fully fit into existing map, "
+                                            "only part will be copied. received: [%lu, %lu], [%lu, %lu] "
+                                            "map is: [0, %lu], [0, %lu]",
+                        x0, xn, y0, yn, costmap_xn, costmap_yn);
+        }
+
+        // update map with data
+        unsigned char *costmap_data = costmap_.getCharMap();
+        size_t i = 0;
+        for (size_t y = y0; y < yn && y < costmap_yn; ++y) {
+            for (size_t x = x0; x < xn && x < costmap_xn; ++x) {
+                size_t idx = costmap_.getIndex(x, y);
+                unsigned char cell_cost = static_cast<unsigned char>(msg->data[i]);
+                costmap_data[idx] = cost_translation_table_[cell_cost];
+                ++i;
+            }
+        }
     }
 
     void poseTopicCallback(PoseWithCovarianceStamped::UniquePtr pose) {
