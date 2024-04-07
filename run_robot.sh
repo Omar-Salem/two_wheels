dd10e13850ecadb95d8131c62a0bea23a3621fcc
omar salem <omar.gamil.salem@gmail.com>
2024-04-07 17:51:28 +1000

    run_robot.sh

--- run_robot.sh
+++ run_robot.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd /home/usr/ros2_ws/
+cd /home/usr/ros2_ws/src
 rm -rf build/ install/ log/
-colcon build --symlink-install
+colcon build
 source install/setup.bash && ros2 launch two_wheels_core two_wheels.launch.py
